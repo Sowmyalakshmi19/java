485ed60bc25298fa4ac9efa3e712f7405132ed84
Sowmyalakshmi19 <Sowmya1904@gmail.com>
2017-01-06 16:27:06 +0530

    Add files via upload

---
+++ reversed.java
@@ -0,0 +1,10 @@
+class reversed{
+public static void main(String[] args){
+String str= new String(" sowmya");
+char[] array=str.toCharArray();
+for(int i=array.length-1;i>=0;i--)
+{
+System.out.print(array[i]);
+}
+}
+}
