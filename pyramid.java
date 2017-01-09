cab7fe591afcbf6a04ac6255b9c7db85ca2edc33
Sowmyalakshmi19 <Sowmya1904@gmail.com>
2017-01-09 17:30:54 +0530

    Add files via upload

---
+++ pyramid.java
@@ -0,0 +1,16 @@
+public class pyramid{
+public static void main(String[] args){
+int k=1;
+if(k==1)
+{
+System.out.print(k + " ");
+}
+for(int i=1;i<=4;i++){
+for(int j=2;j<=i;j++)
+{
+System.out.print(j+1 + " ");
+}
+System.out.println(" ");
+}
+}
+}
