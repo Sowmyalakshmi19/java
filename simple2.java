c484fb80c7f668701270e6e5bfdec4e183a0f81d
Sowmyalakshmi19 <Sowmya1904@gmail.com>
2017-01-10 17:30:49 +0530

    Add files via upload

---
+++ simple2.java
@@ -0,0 +1,13 @@
+public class simple2{
+public static void main(String[]args){
+int x=1;
+int y=1;
+for(int i=0;i<=4;i++){
+for(int j=1;j<=i;j++){
+System.out.print(y + " ");
+y=y+x;
+}
+System.out.println(" ");
+}
+}
+}
